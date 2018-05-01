e8dc5af5f8af2e9fd4a16b88617d3f371490b85c
heyaogui <211920106@qq.com>
2018-05-01 21:44:40 +0800

    chengji

---
+++ com.java/src/com/java/LianXi.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class LianXi {
+
+	public static void main(String[] args) {
+		int score=80;
+		if(score>=90) {
+			System.out.println("优秀");
+		}
+			else if(score>=80) {
+				System.out.println("中等");
+			}
+			else if(score>=70) {
+				System.out.println("良好");
+			}
+			else {    
+	            System.out.println("差");
+	        }
+	}
+}
